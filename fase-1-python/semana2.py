8407d174c34c2d11ce970ff42d357b95fe586875
Gael Antonio Barajas Lopez <gael19.barajas@gmail.com>
2026-06-23 09:27:05 -0600

    Semana 2 Sesion 1 completa

--- fase-1-python/semana2.py
+++ fase-1-python/semana2.py
@@ -1,5 +1,12 @@
 import numpy as np
+#OPERACIONES
+a = np.array([1, 2, 3, 4, 5])
+b = np.array([10, 20, 30, 40, 50])
 
+print(a + b)
+print(a * 2)
+print(a ** 2)
+#SLICING E INDEXING
 m = np.array([[10, 20, 30],
               [40, 50, 60],
               [70, 80, 90]])
@@ -8,3 +15,8 @@ print(m[0, 1])      # un elemento
 print(m[1, :])      # fila completa
 print(m[:, 2])      # columna completa
 print(m[0:2, 0:2])  # submatriz
+#PROPIEDADES
+print(m.shape)
+print(m.ndim)
+print(m.dtype)
+print(m.size)
